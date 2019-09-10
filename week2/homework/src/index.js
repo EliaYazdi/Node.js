cd7c549ed58e129aaec02bb4b637778d25d11402
Elia <elahehahmadiehyazdi@gmail.com>
2019-09-10 08:45:25 +0200

    Reads help and list!

--- week2/homework/src/index.js
+++ week2/homework/src/index.js
@@ -1,3 +1,50 @@
 'use strict';
 
 // TODO: Write the homework code in this file
+const fs = require('fs');
+
+const readList = fs.readFileSync('list.txt', 'utf8', (data, err) => {
+  if (data) {
+    console.log(data);
+  }
+  else if (err) {
+    throw err;
+    console.log('failed!');
+  }
+});
+
+const readHelp = fs.readFileSync('help.txt', 'utf8', (data, err) => {
+  if (data) {
+    console.log(data);
+  }
+  else if (err) {
+    throw err;
+  }
+});
+
+function addList(todos) {
+  fs.writeFile('list.txt', todos, (data, err) => {
+    if (data) {
+      console.log('New task added!');
+    }
+    else if (err) {
+      throw err;
+      console.log('Failed!');
+    }
+  });
+}
+
+const command = process.argv[2]; // add your todo list in command line
+
+function main() {
+  if (command === 'help') {
+    console.log(readHelp);
+  }
+  else if (command === 'list') {
+    console.log(readList);
+  }
+  else if (command === 'add') {
+    addList(command);
+  }
+}
+main();
