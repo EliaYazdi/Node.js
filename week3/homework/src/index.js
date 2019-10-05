645a286c63af8b28a58aa942ab23d2f9433cc5e8
Elia <elahehahmadiehyazdi@gmail.com>
2019-10-05 12:02:26 +0200

    added some todo!

--- week3/homework/src/index.js
+++ week3/homework/src/index.js
@@ -27,7 +27,7 @@ app.post(`/${TODO_SLUG}`,       createTodo.bind(null, todo));
 app.get(`/${TODO_SLUG}`,        readTodos.bind(null, todo));
 app.put(`/${TODO_SLUG}/:id`,    updateTodo.bind(null, todo));
 app.delete(`/${TODO_SLUG}/:id`, deleteTodo.bind(null, todo));
-
+app.get(`/todos/:id`,      readTodos.bind(null, todo));
 
 app.listen(PORT, error => {
   if (error)
